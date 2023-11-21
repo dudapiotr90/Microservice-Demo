f911421c005ce7d9a59e3b0dc06c6e06986f06cc
Piotr Duda <dudapiotr90@gmail.com>
2023-11-21 19:22:31 +0100

    initial commit

---
+++ department-service/src/test/java/pl/dudi/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dudi.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
