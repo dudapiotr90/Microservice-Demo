158ad37f3eca29f924c36477b426d300a450f4bb
Piotr Duda <dudapiotr90@gmail.com>
2023-11-23 18:35:35 +0100

    Added response status

--- department-service/src/main/java/pl/dudi/departmentservice/exception/DepartmentNotFoundException.java
+++ department-service/src/main/java/pl/dudi/departmentservice/exception/DepartmentNotFoundException.java
@@ -1,5 +1,9 @@
 package pl.dudi.departmentservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class DepartmentNotFoundException extends RuntimeException{
     public DepartmentNotFoundException(String message) {
         super(message);
--- employee-service/src/main/java/pl/dudi/employeeservice/exception/EmployeeNotFoundException.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/exception/EmployeeNotFoundException.java
@@ -1,5 +1,9 @@
 package pl.dudi.employeeservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException(String message) {
         super(message);
