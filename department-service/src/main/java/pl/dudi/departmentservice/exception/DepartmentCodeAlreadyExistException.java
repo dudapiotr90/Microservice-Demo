5b4a4595577ae1549ad6dd40d14be61ecfd1495a
Piotr Duda <dudapiotr90@gmail.com>
2023-11-23 18:35:35 +0100

    Added response status

--- department-service/src/main/java/pl/dudi/departmentservice/exception/DepartmentCodeAlreadyExistException.java
+++ department-service/src/main/java/pl/dudi/departmentservice/exception/DepartmentCodeAlreadyExistException.java
@@ -1,5 +1,9 @@
 package pl.dudi.departmentservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class DepartmentCodeAlreadyExistException extends RuntimeException {
     public DepartmentCodeAlreadyExistException(String message) {
         super(message);
--- employee-service/src/main/java/pl/dudi/employeeservice/exception/EmailAlreadyExistException.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/exception/EmailAlreadyExistException.java
@@ -1,5 +1,9 @@
 package pl.dudi.employeeservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class EmailAlreadyExistException extends RuntimeException{
     public EmailAlreadyExistException(String message) {
         super(message);
