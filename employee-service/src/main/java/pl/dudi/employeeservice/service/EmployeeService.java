cb5f13856365f7c33e7f86fecc7dffbbc3aee64b
Piotr Duda <dudapiotr90@gmail.com>
2023-11-24 16:17:59 +0100

    Microservice communication with RestTemplate

--- employee-service/src/main/java/pl/dudi/employeeservice/service/EmployeeService.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/service/EmployeeService.java
@@ -1,10 +1,11 @@
 package pl.dudi.employeeservice.service;
 
+import pl.dudi.employeeservice.dto.APIResponseDto;
 import pl.dudi.employeeservice.dto.EmployeeDto;
 
 public interface EmployeeService {
 
     EmployeeDto saveEmployee(EmployeeDto employeeDto);
 
-    EmployeeDto getEmployee(Long employeeId);
+    APIResponseDto getEmployee(Long employeeId);
 }
--- employee-service/src/main/java/pl/dudi/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -2,7 +2,11 @@ package pl.dudi.employeeservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.dudi.employeeservice.dto.APIResponseDto;
+import pl.dudi.employeeservice.dto.DepartmentDto;
 import pl.dudi.employeeservice.dto.EmployeeDto;
 import pl.dudi.employeeservice.entity.Employee;
 import pl.dudi.employeeservice.exception.EmailAlreadyExistException;
@@ -20,6 +24,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
     private final ModelMapper modelMapper;
+
+    private final RestTemplate restTemplate;
+
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Optional<Employee> optionalEmployee = employeeRepository.findByEmail(employeeDto.getEmail());
@@ -33,12 +40,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 //        return employeeMapper.mapToEmployeeDto(savedEmployee);
         return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
+
     @Override
-    public EmployeeDto getEmployee(Long employeeId) {
+    public APIResponseDto getEmployee(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
-            () -> new EmployeeNotFoundException(String.format("Employee with id:[%s] doesn't exist!",employeeId))
+            () -> new EmployeeNotFoundException(String.format("Employee with id:[%s] doesn't exist!", employeeId))
         );
+        EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
+
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
+            "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+            DepartmentDto.class
+        );
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+
 //        return employeeMapper.mapToEmployeeDto(employee);
-        return modelMapper.map(employee, EmployeeDto.class);
+        return apiResponseDto;
     }
 }
