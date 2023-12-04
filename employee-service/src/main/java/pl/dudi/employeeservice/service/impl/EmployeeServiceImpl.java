112dde47ccc997bdb49e4464d76599c3284b6055
Piotr Duda <dudapiotr90@gmail.com>
2023-12-04 20:03:35 +0100

    Added Retry Patterm

--- employee-service/src/main/java/pl/dudi/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pl.dudi.employeeservice.dto.APIResponseDto;
 import pl.dudi.employeeservice.dto.DepartmentDto;
@@ -21,6 +23,8 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
@@ -42,10 +46,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
 
-    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
-//    @Retry(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("inside getEmployeeById() method");
+
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
             () -> new EmployeeNotFoundException(String.format("Employee with id:[%s] doesn't exist!", employeeId))
         );
@@ -62,6 +68,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public APIResponseDto getDefaultDepartment(Long employeeId,Exception exception) {
+        LOGGER.info("inside getDefaultDepartment() method");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
             () -> new EmployeeNotFoundException(String.format("Employee with id:[%s] doesn't exist!", employeeId))
         );
