f561f444cea002ccf39b684ffa8f897ca26bbc7b
Piotr Duda <dudapiotr90@gmail.com>
2023-12-06 19:09:14 +0100

    added organization-service structure and added repository layer

---
+++ organization-service/src/main/java/pl/dudi/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,10 @@
+package pl.dudi.organizationservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.dudi.organizationservice.entity.Organization;
+
+@Repository
+public interface OrganizationRepository extends JpaRepository<Organization,Long> {
+
+}
