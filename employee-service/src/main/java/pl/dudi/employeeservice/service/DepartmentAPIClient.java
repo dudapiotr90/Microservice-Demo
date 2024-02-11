28709dd57a3e49b6f1245d20a1310c40c629256f
Piotr Duda <dudapiotr90@gmail.com>
2024-02-11 12:19:31 +0100

    to test api commit

--- employee-service/src/main/java/pl/dudi/employeeservice/service/DepartmentAPIClient.java
+++ employee-service/src/main/java/pl/dudi/employeeservice/service/DepartmentAPIClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.dudi.employeeservice.dto.DepartmentDto;
-import pl.dudi.employeeservice.dto.OrganizationDto;
 
 @FeignClient(name = "DEPARTMENT-SERVICE")
 public interface DepartmentAPIClient {
